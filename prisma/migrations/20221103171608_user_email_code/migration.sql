32ce4bb42d0acea775c31e8fe221e7c222647cb3
Sarah Ohlin <sarahohlin1@gmail.com>
2022-11-03 15:48:00 -0400

    fix: conflicting migrations

--- prisma/migrations/20221103171608_user_email_code/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The required column `emailCode` was added to the `User` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
-
-*/
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "emailCode" TEXT NOT NULL;
--- prisma/migrations/20221103171925_6_digit_code_user/migration.sql
+++ prisma/migrations/20221103171925_6_digit_code_user/migration.sql
@@ -5,5 +5,4 @@
 
 */
 -- AlterTable
-ALTER TABLE "User" DROP COLUMN "emailCode",
-ADD COLUMN     "code" TEXT;
+ALTER TABLE "User" ADD COLUMN     "code" TEXT;
